15750bd4471a8b65b74b009d0730cffee24b4514
David Kuseh <davegru203@gmail.com>
2019-11-21 14:40:22 +0000

    initial styling

--- anywherefitness/src/App.js
+++ anywherefitness/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import Nav from './components/nav';
 import MainContent from './components/main';
-import Footer from './components/footer'
+import Footer from './components/footer';
+import Members from './components/members';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <MainContent />
-      < Footer/>
+      <Members />
+      < Footer />
     </div>
   );
 }
--- anywherefitness/src/components/footer.js
+++ anywherefitness/src/components/footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Footer() {
+
+  const Footer = styled.div`
+    text-align: center;
+    margin: 30px;
+    background-color: black;
+    color: white;
+  `
   return (
-    <div className="Footer">
-      Copyright © Anywherefitnessteam
-    </div>
+    <Footer className="Footer">
+      Copyright © Anywhere Fitness Team 2019
+    </Footer>
   );
 }
 
